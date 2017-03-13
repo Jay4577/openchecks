814fe7afc925c0edb19f3950fe47b489f02a618d
sebastienratez <sebastien.ratez@gmail.com>
2017-03-13 11:29:44 +0100

    Adding hack action for save-check-image, using hack docker image with imagemagick

--- actions/save-check-images-hack/deployAction.sh
+++ actions/save-check-images-hack/deployAction.sh
@@ -1,3 +1,4 @@
 export ZIP="save-check-images-hack.zip"
 export IMAGE="sebastienratez/nodejs6action"
+export ACTION="save-check-images-hack"
 base64 $ZIP | echo \"$(cat)\" | jq "{namespace:\"_\", name:\"$ACTION\", exec:{kind:\"blackbox\", image:\"$IMAGE\", code:., binary:true, main:\"main\"}}" | curl -X PUT -H "Content-Type:application/json" -H Authorization:Basic\ $(wsk property get --auth | awk '{print $3}' | base64) -d @- https://openwhisk.ng.bluemix.net/api/v1/namespaces/_/actions/$ACTION?overwrite=true
